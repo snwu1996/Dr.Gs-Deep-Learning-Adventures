7f6bd2e8a8eeabb5f0b8b454f20e555416149901
snwu1996 <shunongwu@gmail.com>
2018-05-24 14:12:42 -0400

    added Data2 class which is an older implementation of the Data class that handles data as 2 arrays one for input data and the other for labels

--- MolHack/src/data_utils.py
+++ MolHack/src/data_utils.py
@@ -87,6 +87,66 @@ class Data:
         self.batch_index = 0
         if shuffle:
             self.shuffle()
+
+class Data2:
+    """
+    The datasets built into Tensorflow allows you to conveniently call the
+    next_batch() function to get the next batch of data.
+    This is just a reimplementation of that function.
+    """
+    def __init__(self, X_data, Y_data):
+        self.X_data = X_data
+        self.Y_data = Y_data
+        self.num_lig_smi = int(X_data.shape[0])
+        self.num_scores = int(Y_data.shape[0])
+        self.batch_num = 0
+    
+    def next_batch(self, batch_size):
+        """
+        Used for gradient descent when the input data set is too large.
+        You can split it up into batches of BATCH_SIZE and iterate through the batches.
+        """
+        X_batch = self.X_data[self.batch_num*batch_size:(self.batch_num+1)*batch_size]
+        Y_batch = self.Y_data[self.batch_num*batch_size:(self.batch_num+1)*batch_size]
+        self.batch_num += 1
+        return X_batch, Y_batch
+    
+    def full_batch(self):
+        """
+        Returns a batch containing all data
+        """
+        return self.X_data, self.Y_data
+    
+    def random_batch(self, batch_size):
+        """
+        Used for stochastic gradient descent.
+        Cuts the dataset into batches of BATCH_SIZE and randomly selects one of those batches
+        """
+        rand_nums = np.random.randint(self.X_data.shape[0], size=(batch_size))
+        X_batch = self.X_data[rand_nums]
+        Y_batch = self.Y_data[rand_nums]
+        return X_batch, Y_batch
+
+    def shuffle(self):
+        """
+        Shuffle the data between every epoch to have faster convergence
+        """
+        new_X = np.empty(self.X_data.shape, dtype=self.X_data.dtype)
+        new_Y = np.empty(self.Y_data.shape, dtype=self.Y_data.dtype)
+        perm = np.random.permutation(self.X_data.shape[0])
+        for old_idx, new_idx in enumerate(perm):
+            new_X[new_idx] = self.X_data[old_idx]
+            new_Y[new_idx]   = self.Y_data[old_idx]
+        self.X_data = new_X
+        self.Y_data = new_Y
+        
+    def reset(self, shuffle=False):
+        """
+        Resets the data. Used after every epoch
+        """
+        self.batch_num = 0
+        if shuffle:
+            self.shuffle()
             
 ########################################################################################
 
